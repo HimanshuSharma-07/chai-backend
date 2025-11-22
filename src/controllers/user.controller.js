c7067eb20d559fc0c18b0bd019ecc4192b389acc
HimanshuSharma-07 <sharmahimanshu82194@gmail.com>
2025-11-22 10:49:47 +0530

    fix: await user retrieval in changeCurrentPassword function in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -238,7 +238,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
     const { oldPassword, newPassword } = req.body
 
-    const user = User.findById(req.user?._id)
+    const user = await User.findById(req.user?._id)
 
 
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
